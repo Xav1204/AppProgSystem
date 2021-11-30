270789c470a32e3aab1de0bbbba5760ec9471211
Xav1204 <73820233+Xav1204@users.noreply.github.com>
2021-11-30 09:42:55 +0100

    fonctions model ok

--- AppProgSystem/Sauvegarde.xaml.cs
+++ AppProgSystem/Sauvegarde.xaml.cs
@@ -9,12 +9,21 @@ namespace AppProgSystem
     /// </summary>
     public partial class Sauvegarde : Window
     {
+        private string valeur_nom;
+        private string valeur_source;
+        private string valeur_cible;
+        private string valeur_type;
+
         Model model = new Model();
 
         public Sauvegarde()
         {
             InitializeComponent();
             Model.set = DataRead;
+            Model.txt_nom = Nom;
+            Model.txt_source = Source;
+            Model.txt_cible = Cible;
+            Model.txt_type = Type;
         }
         private void Read_Button_Click(object sender, RoutedEventArgs e)
         {
@@ -23,26 +32,32 @@ namespace AppProgSystem
 
         private void Create_Button_Click(object sender, RoutedEventArgs e)
         {
+            valeur_nom = Nom.Text;
+            valeur_source = Source.Text;
+            valeur_cible = Cible.Text;
+            valeur_type = Type.Text;
 
+            model.Create(valeur_nom, valeur_source, valeur_cible, valeur_type);
         }
 
         private void Modify_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            model.Modify();
         }
         
         private void Delete_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            model.Delete();
         }
         private void Save_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            valeur_nom = Nom.Text;
+            model.Save(valeur_nom);
         }
 
         private void SequentialSave_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            model.SequentialSave();
         }
 
         private void Chiffrer_Button_Click(object sender, RoutedEventArgs e)
